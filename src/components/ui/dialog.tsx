44052ed0637d9b4bac76e3a02c123f8793e5c81c
sidiamad2012-max <sidiamad2012@gmail.com>
2025-09-30 00:05:46 -0400

    Refactor import statement for React

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react";
+import React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog@1.1.6";
 import { XIcon } from "lucide-react@0.487.0";
 
