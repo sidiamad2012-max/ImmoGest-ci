278e846aa5d5c27a4aa1c7de6b735a9c39cbd03a
sidiamad2012-max <sidiamad2012@gmail.com>
2025-09-30 00:09:03 -0400

    Refactor import statement for React

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { Slot } from "@radix-ui/react-slot@1.1.2";
 import { cva, type VariantProps } from "class-variance-authority@0.7.1";
 
