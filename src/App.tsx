3d39ed16c8ee62ab86011bd387f9628bb8423a21
sidiamad2012-max <sidiamad2012@gmail.com>
2025-09-30 00:40:53 -0400

    Add React import to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { UserRoleProvider, useUserRole } from "./contexts/UserRoleContext.tsx";
 import { NavigationProvider } from "./contexts/NavigationContext";
